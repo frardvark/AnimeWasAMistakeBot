beb63ac1eec3a78f749e97f5d7688a9718776abf
Julian <frardvark@gmail.com>
2018-03-15 19:39:13 -0400

    Anime was a mistake

---
+++ Anime.py
@@ -0,0 +1,19 @@
+import tweepy, time
+
+CONSUMER_KEY = 'ZnFJoAvuD8e0hrBNk1BR2zNdZ'
+CONSUMER_SECRET = 'iAqZBQhfxWhHyJiHzRrJKsv9zPu1qJZqlngDLZmBYEJKtRrHaA'
+ACCESS_TOKEN = '974419809898323974-nY1hcpLYH7IAB9pjxhGmewgXyfDnlUC'
+ACCESS_SECRET = 'jNcG3n22yNuAHyIF3511xYE1PMIbc6SOGloI10gChHdzb'
+
+
+# configure our access information for reaching Twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+
+api = tweepy.API(auth)
+
+while True:
+    api.update_status("Anime was a mistake")
+    print("Tweeting!")
+    time.sleep(90) #Tweet every 2 minutes
+    print("All done tweeting!")
